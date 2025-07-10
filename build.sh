ff4ae270376a53a0d61b149e23118467c9d5dc33
Isaiah <lqlp0011@gmail.com>
2025-07-10 11:51:23 +0800

    toolchains: add build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+# clang downloaded from: https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86
+# linux-x86 is inside toolchains folder
+
+export PATH=~/toolchains/linux-x86/clang-r547379/bin:$PATH
+
+make -j$(nproc --all) O=out ARCH=arm64 vendor/a52sxq_eur_open_defconfig \
+  LLVM=1 \
+  LLVM_IAS=1 \
+  HOSTLD=ld.lld \
+  CC=clang \
+  CROSS_COMPILE=aarch64-linux-gnu- \
+  CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
+
+make -j$(nproc --all) O=out ARCH=arm64 \
+  LLVM=1 \
+  LLVM_IAS=1 \
+  HOSTLD=ld.lld \
+  CC=clang \
+  CROSS_COMPILE=aarch64-linux-gnu- \
+  CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
